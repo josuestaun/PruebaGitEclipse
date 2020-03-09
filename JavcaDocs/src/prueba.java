da242bb77427dbcdd003e56ee7ba322da62dc25d
Josue <josuestaun1@gmail.com>
2020-03-09 10:12:27 +0100

    Segundo commit

--- JavcaDocs/src/prueba.java
+++ JavcaDocs/src/prueba.java
@@ -3,6 +3,6 @@
 public class prueba {
 		//Para probar la subida
 	public static void main(String[] args) {
-		System.out.println("Esto lo subire a mi GitHub");
+		System.out.println("Esto lo subire a mi gitHub");
 	}
 }
